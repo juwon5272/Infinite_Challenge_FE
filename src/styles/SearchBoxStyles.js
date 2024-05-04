7f5e69d8ca6586b3b8098881258a8928c7bef209
juwon5272 <jackson5272@naver.com>
2024-05-04 20:42:07 +0900

    feat($ui): Add styles to submit btn

---
+++ src/styles/SearchBoxStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const BtnContainer = styled.button`
+  width: 48px;
+  height: 48px;
+  border-radius: 48px;
+  cursor: pointer;
+  background-color: rgba(0, 123, 233, 1);
+  border: none;
+  svg {
+    width: 21px;
+    height: 21px;
+  }
+`;
